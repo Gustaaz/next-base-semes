6e64fcea7392a7077e4d65b1002c9775fd403d9a
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-12-20 12:16:54 -0400

    finalizando layout da aplicacao

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,61 +1,17 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { AreaChartInteractive } from "@/components/charts/area-chart-interactive"
-import { BarChartMultiple } from "@/components/charts/bar-chart-multiple"
-import { PieChartDonut } from "@/components/charts/pie-chart-donut"
-import { RadarChartCircle } from "@/components/charts/radar-chart-circle"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { ModeToggle } from "@/components/ui/mode-toggle"
-import { Separator } from "@/components/ui/separator"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
+import { AreaChartInteractive } from "@/components/charts/area-chart-interactive";
+import { BarChartMultiple } from "@/components/charts/bar-chart-multiple";
+import { PieChartDonut } from "@/components/charts/pie-chart-donut";
+import { RadarChartCircle } from "@/components/charts/radar-chart-circle";
 
 export default function Page() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center justify-between border-b px-4">
-          <div className="flex items-center gap-2">
-
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-          </div>
-          <ModeToggle/>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <PieChartDonut />
-            <RadarChartCircle />
-            <BarChartMultiple />
-          </div>
-          
-           <AreaChartInteractive />
-       
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  )
+    <div className="flex flex-1 flex-col gap-4 p-4">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <PieChartDonut />
+        <RadarChartCircle />
+        <BarChartMultiple />
+      </div>
+        <AreaChartInteractive />
+    </div>
+  );
 }
